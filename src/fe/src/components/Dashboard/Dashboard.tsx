a348d0cb4c0d5a959d1d65cf1c37b67eb0830035
Montaga Pallarols <montaga.pallarols@gmail.com>
2023-02-12 23:29:40 +0100

    Isolate form and add states

--- src/fe/src/components/Dashboard/Dashboard.tsx
+++ src/fe/src/components/Dashboard/Dashboard.tsx
@@ -1,111 +1,77 @@
-import React, { useState, useEffect, useCallback, useRef } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import { v4 as uuid } from "uuid";
 
 import Tag from "@components/Tag/Tag";
+import TagForm from "@components/TagForm/TagForm";
 
 import { ITag } from "@interfaces/ITag";
+import { States } from "@enums/states";
 
 import * as S from "./Dashboard.styles";
 
 const Dashboard: React.FC = () => {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [getTagsResponse, setGetTagsResponse] = useState<{
-    status: string;
-    data: ITag[];
-  }>({ status: "idle", data: [] });
-  const [addTagResponse, setAddTagResponse] = useState<string>("idle");
-  const [deleteTagResponse, setDeleteTagResponse] = useState<string>("idle");
-  const [inputValue, setInputValue] = useState<string>("");
+  const [tags, setTags] = useState<ITag[]>([]);
+  const [status, setStatus] = useState<States>(States.IDLE);
+  const [addTagStatus, setAddTagStatus] = useState<States>(States.IDLE);
+  const [deleteTagStatus, setDeleteTagStatus] = useState<States>(States.IDLE);
 
-  const isLoading =
-    getTagsResponse.status === "loading" ||
-    addTagResponse === "loading" ||
-    deleteTagResponse === "loading";
+  const baseUrl = "http://localhost:4000";
+
+  const isLoading = status === "loading";
+  const isError = addTagStatus === "error" || deleteTagStatus === "error";
 
   const fetchData = useCallback(async () => {
+    setStatus(States.LOADING);
     try {
-      const response = await axios.get("http://localhost:4000/tags");
-      setGetTagsResponse({ status: "success", data: response.data });
+      const response = await axios.get(`${baseUrl}/tags`);
+      setTags(response.data);
+      setStatus(States.SUCCESS);
     } catch (error) {
-      console.error(error);
+      setStatus(States.ERROR);
     }
+  }, []);
+
+  useEffect(() => {
+    fetchData();
   }, []);
 
   const addNewTag = useCallback(
     async (inputValue: string) => {
       const id = uuid();
       const newTag = { id: id, value: inputValue };
-      setAddTagResponse("loading");
+      setAddTagStatus(States.LOADING);
       try {
-        await axios.post("http://localhost:4000/tags", newTag);
-        setAddTagResponse("success");
+        await axios.post(`${baseUrl}/tags`, newTag);
+        setAddTagStatus(States.SUCCESS);
         fetchData();
       } catch (error) {
-        setAddTagResponse("error");
-        console.error(error);
+        setAddTagStatus(States.ERROR);
       }
-      setInputValue("");
     },
-    []
+    [fetchData]
   );
 
-  const deleteTag = useCallback(async (id: string) => {
-    setDeleteTagResponse("loading");
-    try {
-      await axios.delete(`http://localhost:4000/tags/${id}`);
-      setDeleteTagResponse("success");
-      setGetTagsResponse({
-        status: "success",
-        data: getTagsResponse.data.filter((tag: ITag) => tag.id !== id),
-      });
-    } catch (error) {
-      setDeleteTagResponse("error");
-      console.error(error);
-    }
-  }, [getTagsResponse]);
-
-  useEffect(() => {
-    fetchData();
-    if (inputRef.current) {
-      inputRef.current.focus();
-    }
-  }, []);
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    addNewTag(inputValue);
-  };
-
+  const deleteTag = useCallback(
+    async (id: string) => {
+      setDeleteTagStatus(States.LOADING);
+      try {
+        await axios.delete(`${baseUrl}/tags/${id}`);
+        setDeleteTagStatus(States.SUCCESS);
+        // Update tags list after delete
+        setTags((prevTags: ITag[]) => prevTags.filter((tag: ITag) => tag.id !== id));
+      } catch (error) {
+        setDeleteTagStatus(States.ERROR);
+      }
+    },
+    [tags]
+  );
 
   return (
     <S.DashboardContainer>
       <S.DashboardHeader>Tag Manager</S.DashboardHeader>
 
-      <S.DashboardTagForm onSubmit={handleSubmit}>
-        <S.DashboardTagInputWrapper>
-          <S.DashboardTagInput
-            ref={inputRef}
-            type="text"
-            name="tag"
-            aria-required="true"
-            aria-label="Add tag"
-            placeholder="Add new tag..."
-            value={inputValue}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setInputValue(e.target.value)
-            }
-          ></S.DashboardTagInput>
-          <S.DashboardTagButton
-            type="submit"
-            title="Add new tag"
-            aria-label="Add tag"
-            disabled={!inputValue}
-          >
-            Add tag
-          </S.DashboardTagButton>
-        </S.DashboardTagInputWrapper>
-      </S.DashboardTagForm>
+      <TagForm onSubmit={addNewTag} />
 
       <S.DashboardTagListHeader>Tags:</S.DashboardTagListHeader>
 
@@ -113,11 +79,13 @@ const Dashboard: React.FC = () => {
         <div>Loading...</div>
       ) : (
         <S.DashboardTagList>
-          {getTagsResponse?.data.map((tag: ITag) => (
+          {tags?.map((tag: ITag) => (
             <Tag key={tag.id} tag={tag} deleteTag={deleteTag} />
           ))}
         </S.DashboardTagList>
       )}
+      
+      {isError && <div>Something went wrong. Please try again.</div>}
     </S.DashboardContainer>
   );
 };
